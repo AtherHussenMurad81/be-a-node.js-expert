78a04e4e5671f169490981398514f55cd16ea956
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-05-11 14:19:48 +0600

    single data add

--- module-6-node.js-rest-api/learining_node/src/controller/product.controller.ts
+++ module-6-node.js-rest-api/learining_node/src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (
   req: IncomingMessage,
@@ -7,7 +8,13 @@ export const productController = (
 ) => {
   const url = req.url;
   const method = req.method;
-  if (url?.startsWith("/products") && method === "GET") {
+
+  const urlPart = url?.split("/");
+  // console.log(urlPart);
+  const id = urlPart && urlPart[1] === "products" ? Number(urlPart[2]) : null;
+  console.log("this is the  id: ", id);
+  // console.log(id);
+  if (url === "/products" && method === "GET") {
     // const data = [
     //   {
     //     id: 56454,
@@ -23,6 +30,18 @@ export const productController = (
       JSON.stringify({
         message: "products page successfully",
         data: data,
+      }),
+    );
+  } else if (method === "GET" && id !== null) {
+    const products = readProduct();
+    const product = products.find((p: IProduct) => p.id === id);
+    console.log(product);
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product retrived successfully",
+        data: product,
       }),
     );
   }
